88dbc963845e53de08a6560bba9582610f3914c0
nanzhi63 <2360372707@qq.com>
2024-05-01 22:30:30 +0800

    update

--- js/navbar.js
+++ js/navbar.js
@@ -6,3 +6,17 @@ $(function() {
         $(".header .center").removeClass("menuopen")
     })
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+    // 获取图片元素
+    var img = document.querySelector('.kanban');
+    
+    // 监听图片的点击事件
+    img.addEventListener('click', function() {
+        // 将页面滚动到顶部
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // 可以使滚动过程更平滑
+        });
+    });
+});
